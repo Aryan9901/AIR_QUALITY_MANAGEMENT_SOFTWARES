4b33f77b3028dd5426db6aa28d1d2bddadb7b5ce
Aryan9901 <911aaryan@gmail.com>
2024-03-16 19:19:59 +0530

    dashboard completed

---
+++ AIR_QUALITY_MONITORING/src/components/chart.jsx
@@ -0,0 +1,204 @@
+/* eslint-disable react/prop-types */
+import {
+	Chart as ChartJS,
+	PointElement,
+	LineElement,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Filler,
+	Legend,
+	ArcElement,
+} from "chart.js";
+import { Bar, Doughnut, Line, Pie } from "react-chartjs-2";
+
+// registering charts componets
+ChartJS.register(Filler, CategoryScale, LineElement, PointElement, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
+
+// export function App() {}
+
+// **???  bar Charts --------------------------------
+
+const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+// exporting bar component
+export const BarChart = ({
+	data_1 = [],
+	data_2 = [],
+	title_1,
+	title_2,
+	bgColor_1,
+	bgColor_2,
+	labels = months,
+	horizontal = false,
+	legend = false,
+	categoryPercentage = 0.6,
+}) => {
+	const options = {
+		responsive: true,
+		indexAxis: horizontal ? "y" : "x",
+		plugins: {
+			legend: {
+				position: "top",
+				display: legend,
+			},
+			title: {
+				// display: true,
+				display: legend,
+				text: `${title_1} & ${title_2}`,
+			},
+		},
+		scales: {
+			y: {
+				beginAtZero: true,
+				grid: {
+					display: false,
+				},
+			},
+			x: {
+				beginAtZero: true,
+				grid: {
+					display: false,
+				},
+			},
+		},
+	};
+
+	const data = {
+		labels,
+		datasets: [
+			{
+				label: title_1,
+				data: data_1,
+				backgroundColor: bgColor_1,
+				barThickness: "flex",
+				barPercentage: 1,
+				categoryPercentage,
+			},
+			{
+				label: title_2,
+				data: data_2,
+				backgroundColor: bgColor_2,
+				barThickness: "flex",
+				barPercentage: 1,
+				categoryPercentage,
+			},
+		],
+	};
+
+	return <Bar width={horizontal ? "200%" : ""} options={options} data={data} />;
+};
+
+// **???  Dougnut Charts --------------------------------
+
+export const DougnutChart = ({ labels, data, backgroundColor, cutout, legends = true, offset }) => {
+	const doughnutData = {
+		labels,
+		datasets: [
+			{
+				data,
+				backgroundColor,
+				borderWidth: 0,
+				offset,
+			},
+		],
+	};
+
+	const doughnutOptions = {
+		responsive: true,
+		plugins: {
+			legend: {
+				display: legends,
+				position: "bottom",
+				labels: {
+					padding: 40,
+				},
+			},
+		},
+		cutout,
+	};
+
+	return <Doughnut data={doughnutData} options={doughnutOptions} />;
+};
+
+// **???  Pie Charts --------------------------------
+
+export const PieChart = ({ labels, data, backgroundColor, legends = true, offset }) => {
+	const pieChartData = {
+		labels,
+		datasets: [
+			{
+				data,
+				backgroundColor,
+				borderWidth: 1,
+				offset,
+			},
+		],
+	};
+
+	const pieChartOptions = {
+		responsive: true,
+		plugins: {
+			legend: {
+				display: legends,
+				position: "bottom",
+				labels: {
+					padding: 40,
+				},
+			},
+		},
+	};
+
+	return <Pie data={pieChartData} hei options={pieChartOptions} />;
+};
+
+// **???  Line Charts --------------------------------
+
+// exporting bar component
+export const LineChart = ({ data, label, backgroundColor, borderColor, labels, legend = false }) => {
+	const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: "top",
+				display: legend,
+			},
+			title: {
+				// display: true,
+				display: legend,
+				text: label,
+			},
+		},
+		scales: {
+			y: {
+				beginAtZero: true,
+				grid: {
+					display: false,
+				},
+			},
+			x: {
+				beginAtZero: true,
+				grid: {
+					display: false,
+				},
+			},
+		},
+	};
+
+	const lineChartData = {
+		labels,
+		datasets: [
+			{
+				label,
+				data,
+				backgroundColor,
+				borderColor,
+				fill: true,
+			},
+		],
+	};
+
+	return <Line options={options} data={lineChartData} />;
+};
--- AIR_QUALITY_MONITORING/src/pages/Dashboard.jsx
+++ AIR_QUALITY_MONITORING/src/pages/Dashboard.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import AdminSidebar from "../components/AdminSidebar";
-import { HiTrendingUp, HiTrendingDown } from "react-icons/hi";
-import { IoIosPartlySunny, IoIosSettings } from "react-icons/io";
+// import { HiTrendingUp, HiTrendingDown } from "react-icons/hi";
+import { IoIosPartlySunny } from "react-icons/io";
 import Bar from "../components/Bar";
 import { useEffect, useState } from "react";
 import { FaLocationArrow } from "react-icons/fa";
@@ -12,6 +12,7 @@ import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import axios from "axios";
 import { useGeolocated } from "react-geolocated";
+import { LineChart, PieChart } from "../components/chart";
 
 // getCountryNames
 const Dashboard = () => {
@@ -20,6 +21,7 @@ const Dashboard = () => {
 	const [query, setQuery] = useState("");
 	const [searchedLat, setSearchedLat] = useState("");
 	const [searchedLong, setSearchedLong] = useState("");
+	const [currCity, setCurrCity] = useState("");
 	const [weather, setWeather] = useState({
 		temp: "",
 		humidity: "",
@@ -59,7 +61,6 @@ const Dashboard = () => {
 			const { data } = await axios.get(
 				`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=9c4a22c9216a8211f581788962507af5&units=metric`
 			);
-			// console.log(data);
 			setWeather({
 				temp: data.main.temp,
 				humidity: data.main.humidity,
@@ -71,6 +72,12 @@ const Dashboard = () => {
 		if (query) {
 			fetchWeatherData();
 		}
+
+		const fetchCity = async () => {
+			const { data } = await axios.get("https://ipinfo.io/?token=");
+			setCurrCity(data.city);
+		};
+		fetchCity();
 	}, [query]);
 
 	useEffect(() => {
@@ -78,7 +85,6 @@ const Dashboard = () => {
 			const { data } = await axios.get(
 				`http://api.openweathermap.org/data/2.5/air_pollution?lat=${searchedLat}&lon=${searchedLong}&appid=9c4a22c9216a8211f581788962507af5`
 			);
-			// console.log(data);
 			setCityaqi(data.list[0].main.aqi);
 			setCityPollutant(data.list[0].components);
 		};
@@ -92,82 +98,109 @@ const Dashboard = () => {
 			<AdminSidebar />
 			<main className="dashboard">
 				<Bar query={query} setQuery={setQuery} />
-				{/* <section className="widget-container">
-					<WidgetItem percent={2.8} value={340000} heading="Income" color="rgba(0,115,255)" />
-					<WidgetItem percent={-2.5} value={400} heading="Paid" color="rgba(0,198,202)" />
-					<WidgetItem percent={4} value={23000} heading="Invoices" color="rgba(0,115,255)" />
-				</section> */}
-				{/* <section className="countries">
-					<select>
-						{countries?.map((country) => (
-							<option key={country.country} value={country.country}>
-								{country.country}
-							</option>
-						))}
-					</select>
-				</section> */}
-				<section className="mapContainer">
-					<div className="HomeCard">
-						<div className="header">
-							<h3>
-								<FaLocationArrow />
-								Home
-							</h3>
-							<button className={cityaqi > 10 ? "redbg" : aqi >= 5 ? "purplebg" : "greenbg"}>
-								{aqi > 10 ? "Severe" : aqi >= 5 ? "Moderate" : "Mild"}
-							</button>
-						</div>
-						<div className="maindata">
-							<div className="graph">
-								<CircularProgressbar value={cityaqi} text={`${cityaqi}`} />
-
-								<div className="g1">
-									<IoIosPartlySunny /> {weather.temp}C
-								</div>
-								<div className="g2">
-									<MdWaterDrop /> {weather.humidity}
-								</div>
-							</div>
-							<div className="data">
-								{cityPollutant && (
-									<>
-										<AqiLevel value={cityPollutant.pm2_5} unit="ug/m^2" parameter="PM 2.5" color={"yellow"} />
-										<AqiLevel value={cityPollutant.co} unit="ug/m^2" parameter="PM 2.5" color={"red"} />
-										<AqiLevel value={cityPollutant.no2} unit="ug/m^2" parameter="PM 2.5" color={"green"} />
-										<AqiLevel value={cityPollutant.pm10} unit="ug/m^2" parameter="PM 2.5" color={"salmon"} />
-										<AqiLevel value={cityPollutant.so2} unit="ug/m^2" parameter="PM 2.5" color={"cyan"} />
-										<AqiLevel value={cityPollutant.no2} unit="ug/m^2" parameter="PM 2.5" color={"cyan"} />
-									</>
-								)}
-							</div>
-						</div>
-					</div>
-					<section className="cityCard">
-						<div className="header">
-							<h3>
-								<FaLocationArrow />
-								Home
-							</h3>
-							<button className={cityaqi > 10 ? "redbg" : aqi >= 5 ? "purplebg" : "greenbg"}>
-								{aqi > 10 ? "Severe" : aqi >= 5 ? "Moderate" : "Mild"}
-							</button>
-						</div>
-						<p>AQI trend in last 24 hrs</p>
-						<div className="maindata">
-							<div className="graph">
-								<CircularProgressbar value={aqi} text={`${aqi}`} />
-							</div>
-							<div className="data">
-								<AqiLevel value={pollutants.pm2_5} unit="ug/m^2" parameter="PM 2.5" color={"yellow"} />
-								<AqiLevel value={pollutants.co} unit="ug/m^2" parameter="PM 2.5" color={"red"} />
-								<AqiLevel value={pollutants.no2} unit="ug/m^2" parameter="PM 2.5" color={"green"} />
-								<AqiLevel value={pollutants.pm10} unit="ug/m^2" parameter="PM 2.5" color={"salmon"} />
-								<AqiLevel value={pollutants.so2} unit="ug/m^2" parameter="PM 2.5" color={"cyan"} />
-								<AqiLevel value={pollutants.no2} unit="ug/m^2" parameter="PM 2.5" color={"cyan"} />
-							</div>
-						</div>
+				<div className="aqicnt">
+					<section className="widget-container">
+						<WidgetItem
+							data={[
+								cityPollutant?.pm2_5,
+								cityPollutant?.co,
+								cityPollutant?.no2,
+								cityPollutant?.so2,
+								cityPollutant?.nh3,
+								cityPollutant?.pm10,
+							]}
+							heading={query}
+						/>
+						<WidgetItem heading={currCity || ""} color="rgba(0,198,202)" data={[38, 306, 10, 6, pollutants?.nh3, pollutants?.pm10]} />
+						{/* <WidgetItem percent={4} value={23000} heading="Invoices"  /> */}
 					</section>
-				</section>
+					<section className="mapContainer">
+						<div className="HomeCard">
+							<div className="header">
+								<h3>
+									<FaLocationArrow />
+									{query}
+								</h3>
+								<button className={cityaqi > 10 ? "redbg" : aqi >= 5 ? "purplebg" : "greenbg"}>
+									{aqi > 10 ? "Severe" : aqi >= 5 ? "Moderate" : "Mild"}
+								</button>
+							</div>
+							<div className="maindata">
+								<div className="graph">
+									<CircularProgressbar value={cityaqi} text={`${cityaqi}`} />
+
+									<div className="g1">
+										<IoIosPartlySunny /> {weather.temp}C
+									</div>
+									<div className="g2">
+										<MdWaterDrop /> {weather.humidity}
+									</div>
+								</div>
+								<div className="data">
+									{cityPollutant && (
+										<>
+											<AqiLevel value={cityPollutant.pm2_5} unit="ug/m^2" parameter="PM 2.5" color={"yellow"} />
+											<AqiLevel value={cityPollutant.co} unit="ug/m^2" parameter="CO" color={"red"} />
+											<AqiLevel value={cityPollutant.no2} unit="ug/m^2" parameter="NO2" color={"green"} />
+											<AqiLevel value={cityPollutant.pm10} unit="ug/m^2" parameter="PM10" color={"salmon"} />
+											<AqiLevel value={cityPollutant.so2} unit="ug/m^2" parameter="SO2" color={"cyan"} />
+											<AqiLevel value={cityPollutant.nh3} unit="ug/m^2" parameter="NH3" color={"cyan"} />
+										</>
+									)}
+								</div>
+							</div>
+							<div className="linegraphdata">
+								<LineChart
+									data={[cityPollutant?.pm2_5, cityPollutant?.no2, cityPollutant?.pm10, cityPollutant?.so2, cityPollutant?.nh3]}
+									legend="true"
+									labels={["PM 2.5", "NO2", "PM10", "SO2", "NH3"]}
+									backgroundColor={"#f8000087"}
+									borderColor={"blue"}
+									label={"Pollutants Level"}
+								/>
+							</div>
+							<div className="piechartdata">
+								<PieChart
+									data={[cityPollutant?.pm2_5, cityPollutant?.no2, cityPollutant?.pm10, cityPollutant?.so2, cityPollutant?.nh3]}
+									labels={["PM 2.5", "NO2", "PM10", "SO2", "NH3"]}
+									backgroundColor={["yellow", "#f8000087", "green", "blue", "#ffc44c", "purple"]}
+									borderColor={"blue"}
+								/>
+							</div>
+						</div>
+						<section className="cityCard">
+							<div className="header">
+								<h3>
+									<FaLocationArrow />
+									Home
+								</h3>
+								<button className={cityaqi > 10 ? "redbg" : aqi >= 5 ? "purplebg" : "greenbg"}>
+									{aqi > 10 ? "Severe" : aqi >= 5 ? "Moderate" : "Mild"}
+								</button>
+							</div>
+							<p>AQI trend in last 24 hrs</p>
+							<div className="maindata" style={{ marginBottom: "2rem" }}>
+								<div className="graph">
+									<CircularProgressbar value={aqi} text={`${aqi}`} />
+								</div>
+								<div className="data">
+									<AqiLevel value={pollutants.pm2_5} unit="ug/m^2" parameter="PM 2.5" color={"yellow"} />
+									<AqiLevel value={pollutants.co} unit="ug/m^2" parameter="CO" color={"red"} />
+									<AqiLevel value={pollutants.no2} unit="ug/m^2" parameter="NO2" color={"green"} />
+									<AqiLevel value={pollutants.pm10} unit="ug/m^2" parameter="PM10" color={"salmon"} />
+									<AqiLevel value={pollutants.so2} unit="ug/m^2" parameter="SO2" color={"cyan"} />
+									<AqiLevel value={pollutants.nh3} unit="ug/m^2" parameter="NH3" color={"cyan"} />
+								</div>
+							</div>
+							<PieChart
+								data={[pollutants?.pm2_5, pollutants?.no2, pollutants?.pm10, pollutants?.so2, pollutants?.nh3]}
+								labels={["PM 2.5", "NO2", "PM10", "SO2", "NH3"]}
+								backgroundColor={["yellow", "#f8000087", "green", "blue", "#ffc44c", "purple"]}
+								borderColor={"blue"}
+							/>
+						</section>
+					</section>
+				</div>
 			</main>
 		</div>
 	);
@@ -185,19 +218,79 @@ export const AqiLevel = ({ value, unit, parameter, color, max = 100 }) => {
 	);
 };
 
-export const WidgetItem = ({ heading, value, percent }) => {
-	const [selectedOption, setSelectedOption] = useState("Today");
-	const handleOptionChange = (e) => {
-		setSelectedOption(e.target.value);
+// const calculateAqi = (data) => {
+// 	// Define national standards for each pollutant
+// 	const nationalStandards = {
+// 		pm25: 25,
+// 		co: 10,
+// 		no2: 40,
+// 		so2: 20,
+// 		nh3: 25,
+// 		pm10: 50,
+// 	};
+
+// 	// Calculate AQI for each pollutant
+// 	const calculatePollutantAqi = (pollutant, value) => {
+// 		const standard = nationalStandards[pollutant];
+// 		return Math.round((value / standard) * 100);
+// 	};
+
+// 	// Calculate AQI for each pollutant in the data array
+// 	const aqiValues = data.map((value, index) => {
+// 		const pollutant = ["pm25", "co", "no2", "so2", "nh3", "pm10"][index];
+// 		return calculatePollutantAqi(pollutant, parseFloat(value));
+// 	});
+
+// 	// Return the maximum AQI value
+// 	return Math.max(...aqiValues);
+// };
+
+const calculateAqi = (data) => {
+	// Define AQI breakpoints and corresponding ranges for each pollutant
+	const aqiBreakpoints = {
+		pm25: [0, 12, 35.4, 55.4, 150.4, 250.4, 350.4, 500.4],
+		pm10: [0, 55, 155, 255, 355, 425, 505, 605],
+		co: [0, 4.4, 9.4, 12.4, 15.4, 30.4, 40.4, 50.4],
+		no2: [0, 53, 100, 360, 649, 1249, 1649, 2049],
+		so2: [0, 35, 75, 185, 304, 604, 804, 1004],
+		nh3: [0, 53, 100, 360, 649, 1249, 1649, 2049],
 	};
+
+	// Calculate AQI for each pollutant
+	const calculatePollutantAqi = (pollutant, value) => {
+		const breakpoints = aqiBreakpoints[pollutant];
+		let aqi = 0;
+
+		for (let i = 0; i < breakpoints.length - 1; i++) {
+			if (value >= breakpoints[i] && value <= breakpoints[i + 1]) {
+				aqi = Math.round(((breakpoints[i + 1] - breakpoints[i]) / (breakpoints[i + 1] - breakpoints[i])) * (i + 1));
+				break;
+			}
+		}
+		return aqi;
+	};
+
+	// Calculate AQI for each pollutant in the data array
+	const aqiValues = data.map((value, index) => {
+		const pollutant = ["pm25", "pm10", "co", "no2", "so2", "nh3"][index];
+		return calculatePollutantAqi(pollutant, parseFloat(value));
+	});
+
+	// Return the maximum AQI value
+	return Math.max(...aqiValues);
+};
+
+// Test the function
+const data = [23, 35, 2.5, 89, 20, 49]; // Example data, replace with your actual data
+const aqi = calculateAqi(data);
+console.log("AQI:", aqi);
+
+export const WidgetItem = ({ heading, aqivalue, data }) => {
 	return (
 		<article className="widget">
 			<div>
-				<i>
-					<IoIosSettings />
-				</i>
 				<h4>{heading}</h4>
-				<p>
+				{/* <p>
 					<select value={selectedOption} onChange={handleOptionChange}>
 						<option value="Today">Today</option>
 						<option value="LastWeek">Last Week</option>
@@ -205,18 +298,10 @@ export const WidgetItem = ({ heading, value, percent }) => {
 						<option value="LastYear">Last Year</option>
 						<option value="All">All</option>
 					</select>
-				</p>
+				</p> */}
 			</div>
-			<h2>&#8377; {Math.abs(value)}</h2>
-			{percent > 0 ? (
-				<h5 className="green">
-					<HiTrendingUp /> + {percent}%
-				</h5>
-			) : (
-				<h5 className="red">
-					<HiTrendingDown /> {Math.abs(percent)}%
-				</h5>
-			)}
+			<h2>{Math.abs(calculateAqi(data))}</h2>
+			<h3>{calculateAqi(data)}</h3>
 		</article>
 	);
 };
