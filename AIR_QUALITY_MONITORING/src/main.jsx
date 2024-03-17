eff82ab4912240c01a51ee3ae47e7558e64940fe
Aryan9901 <911aaryan@gmail.com>
2024-03-17 06:43:37 +0530

    translate bug fixes

--- AIR_QUALITY_MONITORING/src/main.jsx
+++ AIR_QUALITY_MONITORING/src/main.jsx
@@ -1,11 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/app.scss";
-import i18n from "./i18n";
-import { I18nextProvider } from "react-i18next";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<I18nextProvider i18n={i18n}>
-		<App />
-	</I18nextProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
